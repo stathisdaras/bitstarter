7b0b1e6bc19964397e088b226799a1768e78a95e
Dariskos <root@stathisPC.(none)>
2013-07-06 19:57:48 +0300

    Modified web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
+
+var infile = "index.html";
+var msg = fs.readFileSync(infile, 'utf8');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello Sporgiti how are you..??!');
+  response.send(msg);
 });
 
 var port = process.env.PORT || 5000;
